fea2040a285cfd4c67e656cfa0918b85ae70e22c
Zena B <zena667@gmail.com>
2013-07-12 16:25:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinefy::Application.config.secret_token = '351f421917f765c17c0c7e36f5d70f8e99aafd0604f1c4f453b9e8dd61fb7d8010e1c1b1c5c836b10c418d649701fd195ba555c608eb5cb13a12b019c8a2db86'
